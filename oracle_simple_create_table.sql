e5df2455721974e77b06f578330c0c1dffb36e0e
rodolfo.ogaki <rodolfogaki@gmail.com>
2021-09-16 23:36:53 -0300

    inclusão de valor e percentual

--- oracle_simple_create_table.sql
+++ oracle_simple_create_table.sql
@@ -8,6 +8,8 @@ CREATE TABLE produto (
     id_produto NUMBER(11) NOT NULL,
     id_categoria NUMBER(11) NOT NULL,
     descricao VARCHAR2(100) NOT NULL,
+    valor_compra NUMBER(11) NOT NULL,
+    percentual_lucro NUMBER(11) NOT NULL,
     PRIMARY KEY (id_produto),
     CONSTRAINT FK_id_categoria FOREIGN KEY (id_categoria)
     REFERENCES produto_categoria(id_categoria)
