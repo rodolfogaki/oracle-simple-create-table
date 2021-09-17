63175e06edca5729d4e90f0584171b99d85b36e5
rodolfo.ogaki <rodolfogaki@gmail.com>
2021-09-16 23:29:25 -0300

    first commit

---
+++ oracle_simple_create_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE produto_categoria (
+    id_categoria NUMBER(11) NOT NULL,
+    descricao VARCHAR2(100) NOT NULL,
+    PRIMARY KEY (id_categoria)
+);
+
+CREATE TABLE produto (
+    id_produto NUMBER(11) NOT NULL,
+    id_categoria NUMBER(11) NOT NULL,
+    descricao VARCHAR2(100) NOT NULL,
+    PRIMARY KEY (id_produto),
+    CONSTRAINT FK_id_categoria FOREIGN KEY (id_categoria)
+    REFERENCES produto_categoria(id_categoria)
+);
